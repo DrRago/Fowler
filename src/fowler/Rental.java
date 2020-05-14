238272632bddc56ff169927012274ef9dfedecaf
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-05-14 13:13:26 +0200

    reformat coding style

--- src/fowler/Rental.java
+++ src/fowler/Rental.java
@@ -3,13 +3,16 @@ package fowler;
 class Rental {
     private final Movie movie;
     private final int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
     }
