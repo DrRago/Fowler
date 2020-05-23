26fbb0b55134cc05265df771c4d97a1ac96b1404
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-05-23 19:21:38 +0200

    add javadoc

--- src/fowler/Movie.java
+++ src/fowler/Movie.java
@@ -4,19 +4,41 @@ public class Movie {
     private final String title;
     private final Price price;
 
+    /**
+     * Construct a new movie
+     *
+     * @param newTitle the title of the movie
+     * @param price    the price object for the rental
+     */
     public Movie(String newTitle, Price price) {
         title = newTitle;
         this.price = price;
     }
 
+    /**
+     * Check whether the movie price is a new release
+     *
+     * @return true if it is a new release
+     */
     public boolean isNewRelease() {
         return price instanceof NewReleasePrice;
     }
 
+    /**
+     * Calculate the cost of a rental for a given amount of days
+     *
+     * @param rentalDays the days to calculate the cost for
+     * @return the cost
+     */
     public double calculateCostForDays(int rentalDays) {
         return price.calculateCostForDays(rentalDays);
     }
 
+    /**
+     * Get the title of the movie
+     *
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
